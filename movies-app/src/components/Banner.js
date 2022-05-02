0cc866dc4c074e305a064d84f49b9e6eca7ef7d1
Anusha Gupta <guptaanusha1312@gmail.com>
2022-05-03 01:55:30 +0530

    pagination

--- movies-app/src/components/Banner.js
+++ movies-app/src/components/Banner.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 
 export default class Banner extends Component {
     render() {
+        let backdrop = '/6EdKBYkB1ssgGjc249ud1L55o8d.jpg'
         return (
             <div className="card banner-card" >
-                <img src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg" className="card-img-top banner-img" alt="..." />
+                <img src={`https://image.tmdb.org/t/p/original${backdrop}`}
+                    className="card-img-top banner-img" alt="..." />
                 {/* <div className="card-body"> */}
                 <h5 className="card-title banner-title">Black Panther</h5>
                 <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                
+
                 {/* </div> */}
             </div>
         )
--- movies-app/src/components/MovieList.js
+++ movies-app/src/components/MovieList.js
@@ -4,17 +4,55 @@ import { movies } from '../movieData'
 export default class MovieList extends Component {
   render() {
     let moviesArr = movies.results
-    console.log(moviesArr)
+    // console.log(moviesArr)
     return (
-      <div>
-        {
-          moviesArr.map((movieElem) => (
-            <h1>{movieElem.title}</h1>
-          )
+      // <div>
+      //   {
+      //     moviesArr.map((movieElem) => (
+      //       <h1>{movieElem.title}</h1>
+      //     )
 
-          )
-        }
-      </div>
+      //     )
+      //   }
+      // </div>
+      <>
+        <div>
+          <h3 className="text-center">
+            <strong>Trending</strong>
+          </h3>
+        </div>
+
+        <div className='movies-list'>
+          {
+            moviesArr.map((moviesElem) => (
+              <div className="card movie-card" >
+                <img src={`https://image.tmdb.org/t/p/original${moviesElem.backdrop_path}`}
+                  className="card-img-top movie-img" style={{ height: '40vh', width: '20vw' }} alt="..." />
+
+                <p className="card-title movie-title">{moviesElem.title}</p>
+
+                <div className='button-wrapper' style={{ display: 'flex', justifyContent: 'center' }}>
+                  <a href="#" className="btn btn-primary movie-button text-center">Add to Favourites</a>
+                </div>
+
+
+              </div>
+            ))
+          }
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <nav aria-label="Page navigation example">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
+        </div>
+
+      </>
     )
   }
 }

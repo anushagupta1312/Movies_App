6bc7b449ce30291a46edfed5615bfc71ab311c57
Anusha Gupta <guptaanusha1312@gmail.com>
2022-05-03 00:24:33 +0530

    titles displayed

--- movies-app/src/components/MovieList.js
+++ movies-app/src/components/MovieList.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import { movies } from '../movieData'
 
 export default class MovieList extends Component {
   render() {
+    let moviesArr = movies.results
+    console.log(moviesArr)
     return (
-      <div>MovieList</div>
+      <div>
+        {
+          moviesArr.map((movieElem) => (
+            <h1>{movieElem.title}</h1>
+          )
+
+          )
+        }
+      </div>
     )
   }
 }

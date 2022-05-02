c19be69fc02369e6a128808bd8ac883a405ec27b
Anusha Gupta <guptaanusha1312@gmail.com>
2022-05-02 23:50:17 +0530

    day1 complete

--- movies-app/src/components/Banner.js
+++ movies-app/src/components/Banner.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 
 export default class Banner extends Component {
-  render() {
-    return (
-      <div>
-          <h1>Banner</h1>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="card banner-card" >
+                <img src="https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg" className="card-img-top banner-img" alt="..." />
+                {/* <div className="card-body"> */}
+                <h5 className="card-title banner-title">Black Panther</h5>
+                <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                
+                {/* </div> */}
+            </div>
+        )
+    }
 }

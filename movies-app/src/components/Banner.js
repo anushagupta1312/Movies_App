a49fa78c756502d9c7e6d84263a77d7f76e50bae
Anusha Gupta <guptaanusha1312@gmail.com>
2022-05-04 01:04:29 +0530

    api called

--- movies-app/src/components/Banner.js
+++ movies-app/src/components/Banner.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import { movies } from '../movieData'
 
 export default class Banner extends Component {
     render() {
-        let backdrop = '/6EdKBYkB1ssgGjc249ud1L55o8d.jpg'
+        let movieElem = movies.results[Math.floor((Math.random() * 10) + 1)]
+        let backdrop = movieElem.backdrop_path
         return (
             <div className="card banner-card" >
                 <img src={`https://image.tmdb.org/t/p/original${backdrop}`}
                     className="card-img-top banner-img" alt="..." />
                 {/* <div className="card-body"> */}
-                <h5 className="card-title banner-title">Black Panther</h5>
+                <h5 className="card-title banner-title">{movieElem.title}</h5>
                 <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
                 {/* </div> */}
--- movies-app/src/components/MovieList.js
+++ movies-app/src/components/MovieList.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import { movies } from '../movieData'
 
 export default class MovieList extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      hover: ''
+    }
+
+
+  }
   render() {
     let moviesArr = movies.results
     // console.log(moviesArr)
@@ -25,16 +34,21 @@ export default class MovieList extends Component {
         <div className='movies-list'>
           {
             moviesArr.map((moviesElem) => (
-              <div className="card movie-card" >
+              <div className="card movie-card" onMouseEnter={() => this.setState({ hover: moviesElem.id })} onMouseLeave={() => this.setState({ hover: '' })}>
                 <img src={`https://image.tmdb.org/t/p/original${moviesElem.backdrop_path}`}
                   className="card-img-top movie-img" style={{ height: '40vh', width: '20vw' }} alt="..." />
 
                 <p className="card-title movie-title">{moviesElem.title}</p>
 
-                <div className='button-wrapper' style={{ display: 'flex', justifyContent: 'center' }}>
-                  <a href="#" className="btn btn-primary movie-button text-center">Add to Favourites</a>
-                </div>
+                {
+                  this.state.hover == moviesElem.id &&
+                  <div className='button-wrapper' style={{ display: 'flex', justifyContent: 'center' }}>
+                    <a href="#" className="btn btn-primary movie-button text-center">Add to Favourites</a>
+                  </div>
 
+                  //&&-> if cond1 sets true then perform the action specified in cond2
+                  //alternative of if
+                }
 
               </div>
             ))

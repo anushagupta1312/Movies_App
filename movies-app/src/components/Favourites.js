908d6872d4a50c16c89a95aa2456d30e1f0befb8
Anusha Gupta <guptaanusha1312@gmail.com>
2022-05-31 16:28:12 +0530

    favourites updated

--- movies-app/src/components/Favourites.js
+++ movies-app/src/components/Favourites.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import { movies } from '../movieData'
 
 export class Favourites extends Component {
     render() {
+        const moviesArr = movies.results
+        console.log(moviesArr)
+        let genreids = { 28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary', 18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Sci-Fi', 10770: 'TV', 53: 'Thriller', 10752: 'War', 37: 'Western' };
+        let tempArr = []
+        moviesArr.map((movieObj) => {
+            if (!tempArr.includes(genreids[movieObj.genre_ids[0]])) {
+                tempArr.push(genreids[movieObj.genre_ids[0]])
+
+            }
+        })
+        tempArr.unshift("All genres")
         return (
             <div className='main'>
                 <div class="row">
                     <div class="col-3">
                         <ul class="list-group genre-selector">
-                            <li class="list-group-item">An item</li>
-                            <li class="list-group-item">A second item</li>
-                            <li class="list-group-item">A third item</li>
-                            <li class="list-group-item">A fourth item</li>
-                            <li class="list-group-item">And a fifth one</li>
+                            {
+                                tempArr.map((genre) => (
+                                    <li class="list-group-item">{genre}</li>
+                                ))
+                            }
                         </ul>
                     </div>
 
@@ -25,30 +37,27 @@ export class Favourites extends Component {
                             <table class="table">
                                 <thead>
                                     <tr>
-                                        <th scope="col">#</th>
-                                        <th scope="col">First</th>
-                                        <th scope="col">Last</th>
-                                        <th scope="col">Handle</th>
+                                        <th scope="col">Title</th>
+                                        <th scope="col">Genre</th>
+                                        <th scope="col">Popularity</th>
+                                        <th scope="col">Rating</th>
+                                        <th scope="col">Delete</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td colspan="2">Larry the Bird</td>
-                                        <td>@twitter</td>
-                                    </tr>
+                                    {
+                                        moviesArr.map((movieElem) => (
+                                            <tr>
+                                                <td><img style={{ 'width': '5rem', 'float': 'left', 'marginRight': '1rem' }} src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`}
+                                                    alt="movieImg" />
+                                                    <th scope="row">{movieElem.title}</th></td>
+                                                <td>{genreids[movieElem.genre_ids[0]]}</td>
+                                                <td>{movieElem.popularity}</td>
+                                                <td>{movieElem.vote_average}</td>
+                                                <td><button type="button" class="btn btn-danger">Delete</button></td>
+                                            </tr>
+                                        ))
+                                    }
                                 </tbody>
                             </table>
                         </div>
